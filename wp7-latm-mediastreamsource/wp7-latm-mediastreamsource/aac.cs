8badbf7e5730da8b32954291b120ecb760c09d38
Abrar <ahmeda8@outlook.com>
2012-11-29 17:46:28 -0800

    misc

---
+++ wp7-latm-mediastreamsource/wp7-latm-mediastreamsource/aac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace wp7_latm_mediastreamsource
+{
+    public class aac
+    {
+        //main aac context
+        struct AACContext
+        {
+ 
+        }
+
+    }
+}
--- wp7-latm-mediastreamsource/wp7-latm-mediastreamsource/aacdec.cs
+++ wp7-latm-mediastreamsource/wp7-latm-mediastreamsource/aacdec.cs
@@ -4,6 +4,16 @@ namespace wp7_latm_mediastreamsource
 {
     public class aacdec
     {
+        const int LOAS_SYNC_WORD = 0x2b7;
 
+        struct LATMContext
+        {
+            int initialized; //initialized after a valid extradata was seen
+            
+            //parser data
+            int audio_mux_version_A; //latm syntax version
+            int frame_length_type; //0/1 variable/fixed frame length
+            int frame_length;
+        }
     }
 }
