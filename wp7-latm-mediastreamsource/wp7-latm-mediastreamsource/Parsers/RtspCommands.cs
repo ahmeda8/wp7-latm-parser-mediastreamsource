cb45db9a7d9be35afb4ab680ee2b279e59ab8069
ahmeda8 <ahmeda8@outlook.com>
2012-12-01 21:48:22 -0800

    finished the rtsp implementation

---
+++ wp7-latm-mediastreamsource/wp7-latm-mediastreamsource/Parsers/RtspCommands.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wp7_latm_mediastreamsource
+{
+    class RtspCommands
+    {
+        private const string RtspVersion = "RTSP/1.0";
+        private string SourceUrl;
+        private int Cseq;
+
+        public string ContentBase { get; set; } //from describe frame
+        public string Stream { get; set; } //from describe frame
+        public string Range { get; set; } //from describe fram
+        public string Session { get; set; } //from setup frame
+        public string SSRC { get; set; } //from setup frame
+        public int ServerPortRtsp { get; set; } //from setup frame
+        public int ServerPortRtcp { get; set; } //from setup frame
+        public int RtpSeq { get; set; } //from play frame
+        public int RtpTime { get; set; } //from play frame
+
+
+        public RtspCommands(string Url)
+        {
+            SourceUrl = Url;
+            Cseq = 0;
+        }
+
+        public string Describe()
+        {
+            string command = "DESCRIBE " + SourceUrl +" "+ RtspVersion +Environment.NewLine
+                           + "CSeq: " + Cseq++ + Environment.NewLine;
+            return command;
+        }
+                
+        public string Setup(int ClientPort)
+        {
+
+            string command = "SETUP " + ContentBase + Stream +" "+RtspVersion + Environment.NewLine 
+                            +"Transport: RTP/AVP/UDP;unicast;client_port=" + ClientPort + "-" + (ClientPort + 1) + ";mode=play" + Environment.NewLine 
+                            +"CSeq: " + Cseq++ + Environment.NewLine;
+            return command;
+        }
+
+        public string Play()
+        {
+
+            string command = "PLAY " + ContentBase + Stream +" "+ RtspVersion + Environment.NewLine 
+                            +"Range:" + Range + Environment.NewLine
+                            +"Session:" + Session + Environment.NewLine
+                            +"CSeq: " + Cseq++ + Environment.NewLine;
+            return command;
+        }
+
+        public string Teardown()
+        {
+            string command = "TEARDOWN " + SourceUrl +" "+ RtspVersion + Environment.NewLine
+                            +"Session:" + Session + Environment.NewLine
+                            +"CSeq: " + Cseq++ + Environment.NewLine;
+            return command;
+        }
+    }
+}
---
+++ wp7-latm-mediastreamsource/wp7-latm-mediastreamsource/Rtsp.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace wp7_latm_mediastreamsource
+{
+    public class Rtsp : IDisposable
+    {
+        private Socket RtspSocket;
+        private SocketAsyncEventArgs RtspEvntArgs;
+        private string RtspUrl;
+        private Uri RtspUri;
+        private RtspCommands RtspMessages;
+
+        private const int RtspPort = 554;
+        private const int MaxBufferSize = 4096;
+
+        private enum State
+        {
+            Connect,
+            Setup,
+            Describe,
+            Play,
+            Pause,
+            Teardown
+        }
+
+        private State CurrentState;
+        
+        public Rtsp(string url)
+        {
+            RtspUrl = url;
+            RtspUri = new Uri(RtspUrl, UriKind.Absolute);
+            RtspMessages = new RtspCommands(RtspUrl);
+            RtspSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            RtspEvntArgs = new SocketAsyncEventArgs();
+            RtspEvntArgs.RemoteEndPoint = new DnsEndPoint(RtspUri.Host, RtspPort);
+            RtspEvntArgs.Completed += RtspEvntArgs_Completed;
+            RtspEvntArgs.SetBuffer(0, MaxBufferSize);
+            
+        }
+
+        void RtspEvntArgs_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            switch (e.LastOperation)
+            {
+                case SocketAsyncOperation.Connect:
+                    SendMessage(RtspMessages.Describe());
+                    CurrentState = State.Describe;
+                    break;
+                case SocketAsyncOperation.Send:
+                    ReceiveMessage();
+                    break;
+                case SocketAsyncOperation.Receive:
+                    string message = Encoding.UTF8.GetString(e.Buffer, 0, e.BytesTransferred);
+                    Match m;
+                    switch (CurrentState)
+                    {
+                        case State.Describe:
+                            string ContentBasePattern = "Content-Base:(.*)"+Regex.Escape(Environment.NewLine);
+                            string RangePattern = "a=range:(.*)" + Regex.Escape(Environment.NewLine);
+                            m = Regex.Match(message,ContentBasePattern);
+                            RtspMessages.ContentBase = m.Value.Substring(13, m.Length-15);
+                            m = Regex.Match(message, RangePattern);
+                            RtspMessages.Range = m.Value.Substring(8, m.Length-11);
+                            RtspMessages.Stream = "trackID=1";
+                            SendMessage(RtspMessages.Setup(10000));
+                            CurrentState = State.Setup;
+                            break;
+                        case State.Setup:
+                            string SessionPattern = "Session:[^;]+;";
+                            string SsrcPattern = "ssrc=(.*)" + Regex.Escape(Environment.NewLine);
+                            string ServerPortPattern = "server_port=[^;]+;";
+                            m = Regex.Match(message, SessionPattern);
+                            RtspMessages.Session = m.Value.Substring(9,8);
+                            m = Regex.Match(message, SsrcPattern);
+                            RtspMessages.SSRC = m.Value.Substring(5, 8);
+                            m = Regex.Match(message, ServerPortPattern);
+                            RtspMessages.ServerPortRtsp = int.Parse(m.Value.Substring(12,5));
+                            RtspMessages.ServerPortRtcp = int.Parse(m.Value.Substring(18,5));
+                            SendMessage(RtspMessages.Play());
+                            CurrentState = State.Play;
+                            break;
+                        case State.Play:
+                            string RtpSeqPattern = "seq=[^;]+;";
+                            string RtpTimePattern = "rtptime=(.*)"+Regex.Escape(Environment.NewLine);
+                            m = Regex.Match(message, RtpSeqPattern);
+                            RtspMessages.RtpSeq =int.Parse(m.Value.Substring(4, m.Length - 5));
+                            m = Regex.Match(message, RtpTimePattern);
+                            RtspMessages.RtpTime = int.Parse(m.Value.Substring(8,m.Length-10));
+                            
+                            break;
+                        case State.Teardown:
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void SendMessage(string msg)
+        {
+            var buffer = Encoding.UTF8.GetBytes(msg + Environment.NewLine);
+            RtspEvntArgs.SetBuffer(buffer, 0, buffer.Length);
+            RtspSocket.SendAsync(RtspEvntArgs);
+        }
+
+        private void ReceiveMessage()
+        {
+            RtspEvntArgs.SetBuffer(new byte[MaxBufferSize], 0, MaxBufferSize);
+            RtspSocket.ReceiveAsync(RtspEvntArgs);
+        }
+
+        public void Play()
+        {
+            CurrentState = State.Connect;
+            RtspSocket.ConnectAsync(RtspEvntArgs);
+        }
+
+        public void Teardown()
+        {
+            SendMessage(RtspMessages.Teardown());
+            CurrentState = State.Teardown;
+        }
+
+        public void Dispose()
+        {
+            Teardown();
+        }
+    }
+}
